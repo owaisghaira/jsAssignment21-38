89ce1d2bfd7b77fbf8a8646c2151efca15be00dc
owaisghaira <owais.ghaira99@gmail.com>
2020-06-21 15:54:54 +0500

    chapter 21-25  q8 done

--- app.js
+++ app.js
@@ -6,13 +6,52 @@
 // alert("welcome "+full)
 
 // chapter # 21-25 Task no. # 2
-var str = prompt("enter your favourite cell phone")
-var numb = str.length
+// var str = prompt("enter your favourite cell phone")
+// var numb = str.length
 
-for  (var i = 0 ; i < numb.length ; i++){
-    if (str.slice(i, i + 2) === " "){
-        alert("No double spaces")
+// for  (var i = 0 ; i < numb ; i++){
+//     if (str.slice(i, i + 3) === " "){
+//         alert("No double spaces")
+//         break;
+//     }
+// }
+// document.write("length is "+ numb)
 
-    }
-}
-document.write("length is "+ numb)
+// chapter # 21-25 Task no. # 3
+
+// var lett = 'pakistani';
+// var ind = lett.indexOf('n')
+// document.write('string is Pakistani <br>');
+// document.write('index of n is '+ind)
+
+// chapter # 21-25 Task no. # 4
+
+
+// var lett = 'hello world';
+// var ind = lett.lastIndexOf('l')
+// document.write('string is hello world <br>');
+// document.write('Last index of l is '+ind)
+
+// chapter # 21-25 Task no. # 5
+
+// var lett = 'pakistani';
+// var ind = lett.charAt(3)
+// document.write('string is Pakistani <br>');
+// document.write('index of i is '+ind)
+
+// chapter # 21-25 Task no. # 6
+
+// var first = prompt("Enter First Name ","your name plz")
+// var last = prompt("Enter last Name ","your last name plz")
+// var full = first.concat(" ",last)
+// alert("welcome "+full)
+
+// chapter # 21-25 Task no. # 7
+
+// var str = 'Hyderabad';
+// var rep = str.replace('Hyder','Islam')
+// document.write(rep)
+
+// chapter # 21-25 Task no. # 8
+
+var message = 'Ali and Sami are best friends. They play cricket and football together.';
